a559d991faccceab9fda8de17436b186c3bb4118
Clayton Littlejohn <56889577+Claytato@users.noreply.github.com>
2022-07-18 20:55:50 -0400

    Restructured

--- detection/detect.py
+++ detection/detect.py
@@ -1,17 +1,3 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Main script to run the object detection routine."""
 import argparse
 import sys
 import time
@@ -22,20 +8,16 @@ from tflite_support.task import processor
 from tflite_support.task import vision
 import utils
 
-
+  # model: Name of the TFLite object detection model.
+  # camera_id: The camera id to be passed to OpenCV.
+  # width: The width of the frame captured from the camera.
+  # height: The height of the frame captured from the camera.
+  # num_threads: The number of CPU threads to run the model.
+  # enable_edgetpu: True/False whether the model is a EdgeTPU model.
+    
 def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
         enable_edgetpu: bool) -> None:
-  """Continuously run inference on images acquired from the camera.
-
-  Args:
-    model: Name of the TFLite object detection model.
-    camera_id: The camera id to be passed to OpenCV.
-    width: The width of the frame captured from the camera.
-    height: The height of the frame captured from the camera.
-    num_threads: The number of CPU threads to run the model.
-    enable_edgetpu: True/False whether the model is a EdgeTPU model.
-  """
-
+ 
   # Variables to calculate FPS
   counter, fps = 0, 0
   start_time = time.time()
