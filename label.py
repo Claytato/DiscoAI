c47e8cd5f2f24732db35c4606735acf685f12884
Clayton Littlejohn <56889577+Claytato@users.noreply.github.com>
2022-07-18 22:24:19 -0400

    Added percentage human detection
    50%

--- label.py
+++ label.py
@@ -19,12 +19,15 @@ def visualize(
     bbox = detection.bounding_box
     start_point = bbox.origin_x, bbox.origin_y
     end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 3)
+    cv2.rectangle(image, start_point, end_point, _TEXT_COLOR, 1)
 
     # Draw label and score
     category = detection.classes[0]
     class_name = category.class_name
     probability = round(category.score, 2)
+    if class_name == 'person':
+        if probability > 0.50:
+          print("person")
     result_text = class_name + ' (' + str(probability) + ')'
     text_location = (_MARGIN + bbox.origin_x,
                      _MARGIN + _ROW_SIZE + bbox.origin_y)
